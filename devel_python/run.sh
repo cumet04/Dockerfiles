874f5bade1beb59ac83d9dff3dc7cc10ba0127ae
medalhkr <medalhkr@gmail.com>
2015-06-03 15:54:10 +0900

    use fedora for python container instead of archlinux

--- devel_python/run.sh
+++ devel_python/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -v /home/medalhkr/project:/home/documents/project --name devel_python medalhkr/devel_python /sbin/init
+docker run -d -v /home/medalhkr/project:/mnt --name devel_python medalhkr/devel_python /sbin/init
